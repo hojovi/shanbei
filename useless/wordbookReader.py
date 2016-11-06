a51426ac860a566dbf2c13ae288aeae54f90a301
hojovi <htjovi@gmail.com>
2016-11-06 15:19:22 +0800

    修改数据库初始化后插入初始数据（word表，tag表）脚本文件bug

--- useless/wordbookReader.py
+++ useless/wordbookReader.py
@@ -1,16 +1,18 @@
 import xmltodict
+import sys
+sys.path.append('../')
 from db import Tag,Word
 
 with open('wordbook.xml',encoding='utf8') as fd:
-    doc=xmltodict.parse(fd.read())
+   doc=xmltodict.parse(fd.read())
 
 for item in doc['wordbook']['item']:
-    num=Tag.query_count(tag=item['tags'])
-    if num==0:
-        tag=Tag(tag=item['tags'])
-        Tag.insert(tag)
-    else:
-        tag=Tag.query_one(tag=item['tags'])
-    word=Word(word=item['word'],explanation=item['trans'])
-    word.tags.append(tag)
-    Word.insert(word)
+   num=Tag.query_count(tag=item['tags'])
+   if num==0:
+       tag=Tag(tag=item['tags'])
+       Tag.insert(tag)
+   else:
+       tag=Tag.query_one(tag=item['tags'])
+   word=Word(word=item['word'],explanation=item['trans'])
+   word.tags.append(tag)
+   Word.insert(word)
