c41b9dba7cac8818e6d6164f280e1cdce51f8179
hojovi <htjovi@gmail.com>
2016-11-06 15:10:01 +0800

    修改404错误处理

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def page_unauthenticated(error):
     return render_template('401.html'),401
 @main.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'),401
+    return render_template('404.html'),404
 
 @main.route('/',methods=['GET'])
 def index():
